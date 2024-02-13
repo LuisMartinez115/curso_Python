952b48573507d7c9e0247f95b3879658ff1bec27
Luis_Martinez115 <101226571+LuisMartinez115@users.noreply.github.com>
2024-02-13 08:44:16 -0600

    Add files via upload

---
+++ condicionales/else-if.py
@@ -0,0 +1,9 @@
+ingresos = 10000
+
+if ingresos > 10000: 
+    print("estas bien en la economia")
+elif ingresos > 5000 :
+    print("tienes economia estable")
+else:
+    print("eres bien pinches pobre")
+
---
+++ condicionales/if-else.py
@@ -0,0 +1,9 @@
+edad = 18
+
+if edad >= 18 : 
+    print("pasaste con 10")
+else: 
+    print("no pasaste, estas en extra")
+print ("ya no es parte de la condicional")
+
+#---------------------------------------------------------------------------------------
