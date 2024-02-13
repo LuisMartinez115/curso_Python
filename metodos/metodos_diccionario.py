ceba8a534164ba0d27840f42a470753140c67757
Luis_Martinez115 <101226571+LuisMartinez115@users.noreply.github.com>
2024-02-13 09:38:34 -0600

    Update metodos_diccionario.py

--- metodos/metodos_diccionario.py
+++ metodos/metodos_diccionario.py
@@ -13,5 +13,20 @@ print (clave)
 valor = dicc.get("casada")
 print (valor)
 #devuelve el elemento dentro de la key en el diccionario
+#si no lo encuentra devuelve "none" indicando que no tiene ningun elemento
 
+#POP
+dicc.pop("edad")
+print(dicc)
+#elimina un valor especifico dentro del diccionario
+
+#ITEMS
+iteracion_dicc = dicc.items()
+print(iteracion_dicc)
+#devuelve una lista donde podremos iterar sobre los elementos
+    
+#CLEAR
+dicc.clear()
+print(dicc)
+#elimina los valores del diccionario
 
