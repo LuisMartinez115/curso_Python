af2c90ed03ed40f2781ef198acdb2c67b8630d17
Luis_Martinez115 <101226571+LuisMartinez115@users.noreply.github.com>
2024-02-13 08:41:43 -0600

    Add files via upload

---
+++ variables/variables.py
@@ -0,0 +1,24 @@
+
+
+#variable con camelCase
+
+NombreCompleto = "luis angel martinez alvarez"
+
+#variable con snake_case
+
+Nombre_completo = "luis angel martinez alvarez"
+
+#concatenar con +
+ 
+bienvenida = "hola " + NombreCompleto + " ¿cómo estas?"
+
+#concatenar con f-string 
+
+bienvenida2 = f"hola {NombreCompleto} ¿cómo estas?"
+
+#operadores de pertenencia (in / not in)
+
+print("luis" in bienvenida) #true
+print("algo" in bienvenida) #false 
+
+#
